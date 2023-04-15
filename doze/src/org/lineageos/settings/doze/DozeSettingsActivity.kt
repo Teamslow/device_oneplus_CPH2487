chandu078/device_oneplus_sm8350-common

sm8350-common: Merge hardware/oplus components
[All credits to related authors on]
- https://github.com/LineageOS/android_hardware_oplus/commits/lineage-19.1.

[Drop]:
- HighTouchPollingRate interface

Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.os.Bundle
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import com.android.settingslib.collapsingtoolbar.R
+
+class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.content_frame, DozeSettingsFragment(), TAG)
+            .commit()
+    }
+
+    companion object {
+        private const val TAG = "DozeSettingsActivity"
+    }
+}

Teamslow/device_oneplus_CPH2487

   * https://github.com/LineageOS/android_hardware_oplus/commits/lineage-20.
   * Doze pocket sensor support
   * KeyHandler customization
   * Import cleaned SEPolicy

doze/src/org/lineageos/settings/doze/DozeSettingsActivity.kt
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.doze
+
+import android.os.Bundle
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
+import com.android.settingslib.widget.R
+
+class DozeSettingsActivity : CollapsingToolbarBaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fragmentManager
+            .beginTransaction()
+            .replace(R.id.content_frame, DozeSettingsFragment(), TAG)
+            .commit()
+    }
+
+    companion object {
+        private const val TAG = "DozeSettingsActivity"
+    }
+}